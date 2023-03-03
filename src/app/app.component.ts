40a33668497e94177d6c5e0fd28405adb8db3ec9
cyrille <aviateur22@hotmail.fr>
2023-03-03 11:31:15 +0100

    Add title restriction for mockApi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,7 +56,7 @@ export class AppComponent {
   setBackendService() {   
     // Selection backend
     const apiServiceImp = new ApiServiceImp(this.backendSourceApiSelection);
-    apiServiceImp.setBackendApi(BackendApiSource.localBackEndApi);
+    apiServiceImp.setBackendApi(BackendApiSource.mockBackendApi);
   }
 
   /**
