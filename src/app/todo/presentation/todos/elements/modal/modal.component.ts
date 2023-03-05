38f75201e5111af45c969eab3d181b6bc741ddb4
cyrille <aviateur22@hotmail.fr>
2023-03-05 14:01:20 +0100

    Complete animation on app

--- src/app/todo/presentation/todos/elements/modal/modal.component.ts
+++ src/app/todo/presentation/todos/elements/modal/modal.component.ts
@@ -1,4 +1,27 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0
+  }),
+  animate('1s ease-in', style({
+    opacity: 1
+  }))
+]);
+
+const exitTransition = transition(':leave', [
+  style({
+    opacity: 1
+  }),
+  animate('1s ease-out', style({
+    opacity: 0
+  }))
+]);
+
+const fadeIn = trigger('fadeIn', [enterTransition]);
+const fadeOut = trigger('fadeOut', [exitTransition]);
+
 
 @Component({
   selector: 'app-modal',
